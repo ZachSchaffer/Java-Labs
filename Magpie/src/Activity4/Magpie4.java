b090409bdf0d1027376c3ffaa4c33b04ea053ca2
cursedhydra <zachanddan@yahoo.com>
2016-05-09 17:24:45 -0400

    Make magpie great again

--- Magpie/src/Activity4/Magpie4.java
+++ Magpie/src/Activity4/Magpie4.java
@@ -72,6 +72,11 @@ public class Magpie4
             {
                 response = transformYouMeStatement(statement);
             }
+            else if (psn >=0
+                    && findKeyword(statement, "I") >= 0)
+            {
+                response = transformIYouStatement(statement);
+            }
             else
             {
                 response = getRandomResponse();
@@ -107,6 +112,7 @@ public class Magpie4
 
     private String transformIWantStatement(String statement)
     {
+        //  Remove the final period, if there is one
         statement = statement.trim();
         String lastChar = statement.substring(statement
                 .length() - 1);
@@ -117,7 +123,7 @@ public class Magpie4
         }
         int psn = findKeyword (statement, "I want", 0);
         String restOfStatement = statement.substring(psn + 6).trim();
-        return "Would it really make you happy if you had " + restOfStatement;
+        return "Would it really make you happy if you had " + restOfStatement + "?";
 
     }
 
@@ -147,6 +153,22 @@ public class Magpie4
         return "What makes you think that I " + restOfStatement + " you?";
     }
 
+    private String transformIYouStatement(String statement) {
+        //  Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement
+                .length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement
+                    .length() - 1);
+        }
+
+        int psnOfI = findKeyword(statement, "I", 0);
+        int psnOfYou = findKeyword(statement, "you", psnOfI + 1);
+
+        String restOfStatement = statement.substring(psnOfI +1, psnOfYou).trim();
+        return "Why do you " + restOfStatement + " me?";
+    }
 
 
 
@@ -230,10 +252,10 @@ public class Magpie4
     }
 
 
-        /**
-         * Pick a default response to use if nothing else fits.
-         * @return a non-committal string
-         */
+    /**
+     * Pick a default response to use if nothing else fits.
+     * @return a non-committal string
+     */
     private String getRandomResponse()
     {
         final int NUMBER_OF_RESPONSES = 4;
